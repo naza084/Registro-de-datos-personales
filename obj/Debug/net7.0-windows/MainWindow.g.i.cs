2712770c707dbbc08e50a48682419aaf1cb8d4dd
naza084 <115745741+naza084@users.noreply.github.com>
2023-12-28 07:42:01 -0300

    Correciónes

--- obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -93,7 +93,7 @@ namespace Registro_de_datos_personales {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Registro de datos personales;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Registro de datos personales;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
